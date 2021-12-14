ebfcb958bf379de08280d74f94a5e8cdfd6d8ba3
Damilare Olaleye <damilare.olaleye@cyatmr.com>
2021-12-13 20:18:13 -0600

    fix bugs On     - login and signup button being editable     - change regex for email validation

--- Reimbursement/src/main/java/com/revature/service/UserService.java
+++ Reimbursement/src/main/java/com/revature/service/UserService.java
@@ -123,7 +123,8 @@ public class UserService implements UserServiceInterface {
 				throw new NotFoundException("user name cannot be blank");
 			}
 
-			if (!(email.matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$"))) {
+			if (!(email.matches(
+					"(?:[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"))) {
 				throw new InvalidParameterException("You entered an invalid email");
 			}
 
